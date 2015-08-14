d93eec5000c0182c5b4013eb8580f2b5178250a2
valduze <jongi@ilikeit.co.za>
2015-08-14 16:03:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         $daysRemaining = floor($datediff/(60*60*24));
         return ($daysRemaining);
     }*/
-    
+
     function getBirthday($birthday) {
         $cur_day = date('Y-m-d');
         $cur_time_arr = explode('-',$cur_day);
@@ -21,7 +21,7 @@
 
         $cur_year_b_day = $cur_time_arr[0]."-".$birthday_arr[1]."-".$birthday_arr[2];
 
-        if(strtotime($cur_year_b_day) < time())
+        /*if(strtotime($cur_year_b_day) < time())
         {
             echo " Birthday already passed this year for ";
         }
@@ -29,12 +29,53 @@
         {
             $diff=strtotime($cur_year_b_day)-time();//time returns current time in seconds
             echo " ". $days=floor($diff/(60*60*24)). " day(s) to go for ";
-        }
+        }*/
+        $diff=strtotime($cur_year_b_day)-time();//time returns current time in seconds
+         $days=floor($diff/(60*60*24));
+        return $days;
     }
- 
-    foreach ($row as $rows) {
+
+    /*foreach ($row as $rows) {
           echo "<pre>";
               echo $rows['fname'].  " ". $rows['lname'] . " " . $rows['dob'] . " ". getBirthday($rows['dob']) . " ".  "\n";
           echo "</pre>";
-    }
-?>
+    }*/
+    //Use CLICKATELL API to send you self an sms a day before your child's birthday
+foreach ($row as $child) {
+        if(getBirthday($child['dob']) == 15)  {
+               $user = "user";
+               $password = "password";
+               $api_id = "apiID";
+               $baseurl ="http://api.clickatell.com";
+
+               $text = urlencode("Tomorrow is  $child[fname]'s birthday...sent by Jongi's Kid's birthdays");
+               $to = "27761052812";
+
+               // auth call
+               $url = "$baseurl/http/auth?user=$user&password=$password&api_id=$api_id";
+
+               // do auth call
+               $ret = file($url);
+
+               // explode our response. return string is on first line of the data returned
+               $sess = explode(":",$ret[0]);
+               if ($sess[0] == "OK") {
+
+                   $sess_id = trim($sess[1]); // remove any whitespace
+                   $url = "$baseurl/http/sendmsg?session_id=$sess_id&to=$to&text=$text";
+
+                   // do sendmsg call
+                   $ret = file($url);
+                   $send = explode(":",$ret[0]);
+
+                   if ($send[0] == "ID") {
+                       echo "successnmessage ID: ". $send[1];
+                   } else {
+                       echo "send message failed";
+                   }
+               } else {
+                   echo "Authentication failure: ". $ret[0];
+               }
+              //echo "Happy birthday!!!"  .$rows['fname']; 
+        }
+   }
